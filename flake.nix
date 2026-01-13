334badfa65348a2c192fedc520a5baa0d8dc64ad
Łukasz Niemier <~@hauleth.dev>
2026-01-13 12:20:08 +0100

    Update Flake

--- flake.nix
+++ flake.nix
@@ -29,10 +29,6 @@
           rustfmt
           socat
           openssl
-        ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-          darwin.apple_sdk.frameworks.CoreFoundation
-          darwin.apple_sdk.frameworks.CoreServices
-          darwin.apple_sdk.frameworks.IOKit
         ];
         };
       };
